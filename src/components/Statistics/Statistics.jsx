148ef73cdce97fc35881dc28c577a2b7e7f3edde
Denis Fedash <0501054104d@gmail.com>
2022-03-05 23:34:13 +0200

    add style

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,22 @@
 import PropTypes from "prop-types";
+import css from "./Statistic.module.css"
 
-
-export const Statistics = ({ title , stats}) => {
+export const Statistics = ({ title, stats }) => {
+    function randCol() {
+    let r =
+      '#' +
+      (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase() +
+      '80';
+    return r;
+  }
     return (
-        <section className="statistics">
-            {title && <h2 className="title">{title}</h2>}
-            <ul className="stat-list">
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+            <ul className={css.statList}>
                 {stats.map(stat =>
-                    <li className="item" key={stat.id}>
-                        <span className="label">{stat.label}</span>
-                        <span className="percentage">{stat.percentage}</span>
+                    <li style={{backgroundColor: randCol() }} className={css.item} key={stat.id}>
+                        <span className={css.label}>{stat.label}</span>
+                        <span className={css.percentage}>{stat.percentage}%</span>
                     </li>
                 )}
                 
