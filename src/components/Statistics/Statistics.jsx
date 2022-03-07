03b1f4438b39c9a7749f240fd392521e332cf398
Denis Fedash <0501054104d@gmail.com>
2022-03-07 15:27:32 +0200

    destructurisation

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -13,10 +13,10 @@ export const Statistics = ({ title, stats }) => {
         <section className={css.statistics}>
             {title && <h2 className={css.title}>{title}</h2>}
             <ul className={css.statList}>
-                {stats.map(stat =>
-                    <li style={{backgroundColor: randCol() }} className={css.item} key={stat.id}>
-                        <span className={css.label}>{stat.label}</span>
-                        <span className={css.percentage}>{stat.percentage}%</span>
+                {stats.map(({id, label, percentage}) =>
+                    <li style={{backgroundColor: randCol() }} className={css.item} key={id}>
+                        <span className={css.label}>{label}</span>
+                        <span className={css.percentage}>{percentage}%</span>
                     </li>
                 )}
                 
